87674b5c12a06d45d6a039c2c6cfa6335fc5ba0c
Paulina Szubarczyk <paulinaszubarczyk@gmail.com>
2015-05-07 00:34:49 +0200

    Robocostam dziala

--- app/src/androidTest/java/com/example/paulina/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/paulina/myapplication/ApplicationTest.java
@@ -1,13 +1,18 @@
 package com.example.paulina.myapplication;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ApplicationTest {
+
+    @Test
+    public void shouldHaveHappySmiles() throws Exception {
+        assertThat("Hello World!", equalTo("Hello World!"));
     }
+
 }
