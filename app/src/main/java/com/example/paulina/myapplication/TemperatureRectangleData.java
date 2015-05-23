529786c74c0c02bcc3aba8a2546c451d90bf3cd7
Adam Kosiorek <kosiorek.adam@gmail.com>
2015-05-23 19:51:05 +0200

    rectangle temp and line gradient analysis

---
+++ app/src/main/java/com/example/paulina/myapplication/TemperatureRectangleData.java
@@ -0,0 +1,10 @@
+package com.example.paulina.myapplication;
+
+public class TemperatureRectangleData {
+    int xMin;
+    int yMin;
+    int xMax;
+    int yMax;
+    float tMin;
+    float tMax;
+}
